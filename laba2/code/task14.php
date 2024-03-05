0dbcfae32bdebf591f35982b37cfb2896bd8025c
Alia <aliya..yarkayeva@bk.ru>
2024-03-05 22:07:02 +0200

    task 14 added

--- laba2/code/task14.php
+++ laba2/code/task14.php
@@ -1,56 +1,4 @@
 <?php
-//task 1
-echo "task 1\n";
-
-$very_bad_unclear_name = "15 chicken wings";
-$order = &$very_bad_unclear_name;
-$order .= " and shefburger";
-echo "Your order is: $very_bad_unclear_name.\n";
-
-//task 2
-echo "\ntask 2\n";
-
-$newInt = 13;
-echo $newInt . "\n";
-
-$newFloat = 1.11;
-echo $newFloat . "\n";
-
-echo 6 + 6 . "\n";
-
-$lastMonth = 1187.23;
-$thisMonth = 1089.98;
-echo $lastMonth - $thisMonth . "\n";
-//task 11
-echo "\ntask 11\n";
-
-$numLanguages = 4;
-$months = 11;
-
-$days = 16 * $months;
-
-$daysPerLanguages = $days / $numLanguages;
-echo "$daysPerLanguages per programming language\n";
-
-
-//task 12
-echo "\ntask 12\n";
-
-$squared = 8 ** 2;
-echo "8^2 = $squared\n";
-
-//task 13
-echo "\ntask 13\n";
-
-$myNum = 52;
-$answer = $myNum;
-$answer += 2;
-$answer *= 2;
-$answer -= 2;
-$answer /= 2;
-$answer -= $myNum;
-echo "answer $answer\n";
-
 //task 14
 echo "\ntask 14\n";
 
@@ -178,8 +126,3 @@ foreach ($array as $item) {
     if ($temp_sum > 10) break;
 }
 echo "we should summarize first $cnt elements to receive sum greater than 10\n";
-
-
-
-
-
