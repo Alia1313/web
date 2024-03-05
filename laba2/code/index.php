bc49cff9664f7e07d5e7755d9a9f924c8eae256d
Alia <aliya..yarkayeva@bk.ru>
2024-03-05 22:56:56 +0200

    task 19 added

--- laba2/code/index.php
+++ laba2/code/index.php
@@ -416,6 +416,14 @@ if (sizeof($arr) == 3) {
     echo "array size = 3 and sum of its elements = $sum" . "\n";
 }
 
+//task 19
+echo "\ntask19 \n";
+for ($i = 1; $i <= 20; ++$i) {
+    for ($j = 0; $j < $i; ++$j) {
+        echo "x";
+    }
+    echo "\n";
+}
 
 
 
---
+++ laba2/code/task19.php
@@ -0,0 +1,9 @@
+<?php
+//task 19
+echo "\ntask19 \n";
+for ($i = 1; $i <= 20; ++$i) {
+    for ($j = 0; $j < $i; ++$j) {
+        echo "x";
+    }
+    echo "\n";
+}
