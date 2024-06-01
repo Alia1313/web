bdc74e9ff0550f5e6e3ecb6496340c76b481b873
Alia <aliya..yarkayeva@bk.ru>
2024-06-01 21:15:39 +0200

    update save.php for writing in database

--- laba5/code/save.php
+++ laba5/code/save.php
@@ -1,5 +1,4 @@
 <?php
-include 'vendor/autoload.php';
 
 function redirectToHome(): void 
 {
@@ -17,21 +16,14 @@ $category = $_POST['category'];
 $title = $_POST['title'];
 $desc = $_POST['description'];
 
-$client = new Google_Client();
-$client->setApplicationName('Google Sheets API');
-$client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
-$client->setAccessType('offline');
-$path = "civil-density-421619-c45e530196f3.json";
-$client->setAuthConfig($path);
-$service = new Google_Service_Sheets($client);
-$spreadsheetId = '1J2LJpuwXbOBdOPKgF-SjFpIg5fpfy4NubTCXRCetG1s';
-$newRow = [$email, $category, $title, $desc];
-$rows = [$newRow]; 
-$valueRange = new Google_Service_Sheets_ValueRange();
-$valueRange->setValues($rows);
-$range = 'Sheet1';
-$options = ['valueInputOption' => 'USER_ENTERED'];
-$service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $options);
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_errno()) {
+    printf('Подключение к серверу MySQL невозможно. Код ошибки: %s\n', mysqli_connect_error());
+    exit;
+} else {
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES('$email', '$title', '$desc', '$category')");
+}
 
 redirectToHome();
 
